860fba9094e4dedd3061f3c0cc0eb885b6ac3259
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-03-04 21:48:06 +0530

    reformatted MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,110 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles/App.css';
 import Keyboard from './components/Keyboard';
 import { wordList } from './utils';
 
 const App: React.FC = () => {
   const [keyboardKey, setKeyboardKey] = useState(0);
-  const [currentWordObj, setCurrentWordObj] = useState(
+  const [userInput, setUserInput] = useState<string | null>();
+  const [attempts, setAttempts] = useState(7);
+  const [wordObj, setWordObj] = useState(
     wordList[Math.floor(Math.random() * 850)]
   );
+  const [charLeft, setCharLeft] = useState(wordObj.word.split(''));
+
+  const charSpans = document.getElementsByClassName(
+    'char-span'
+  ) as HTMLCollectionOf<HTMLSpanElement>;
 
   function handleRefresh() {
-    setKeyboardKey((prev) => prev + 1);
-    setCurrentWordObj(wordList[Math.floor(Math.random() * 850)]);
+    setWordObj(wordList[Math.floor(Math.random() * 850)]);
+    setKeyboardKey((prev) => prev + 1); // to remount the Keyboard component
+    setAttempts(7);
+    setCharLeft(wordObj.word.split(''));
+    for (let i = 0; i < charSpans.length; i++) {
+      const charSpan = charSpans.item(i);
+      if (charSpan) {
+        charSpan.style.display = 'none';
+        charSpan.style.color = 'red';
+      }
+    }
   }
 
+  function handleUserInput(input: string) {
+    input = input.toUpperCase();
+    const charBtn = document.getElementById(`${input}-btn`);
+
+    if (charLeft.includes(input.toLowerCase())) {
+      // Correct guess logic
+
+      // reveal the character occurence
+      for (let i = 0; i < charSpans.length; i++) {
+        const charSpan = charSpans.item(i);
+        if (charSpan && charSpan.innerText == input) {
+          charSpan.style.display = 'inline-block';
+          charSpan.style.color = 'green';
+        }
+      }
+
+      // update the charleft array
+      setCharLeft(charLeft.filter((char) => char != input.toLowerCase()));
+
+      charBtn?.setAttribute('disabled', 'true');
+    } else {
+      // Incorrect guess logic
+      setAttempts((prev) => prev - 1);
+      charBtn?.setAttribute('disabled', 'true');
+    }
+  }
+
+  useEffect(() => {
+    if (userInput) charLeft.length && attempts && handleUserInput(userInput);
+  }, [userInput]);
+
+  useEffect(() => {
+    if (!attempts)
+      for (let i = 0; i < charSpans.length; i++) {
+        const charSpan = charSpans.item(i);
+        if (charSpan) charSpan.style.display = 'inline-block';
+      }
+  }, [attempts]);
+
   return (
-    <div
-      style={{
-        width: '90vw',
-        height: '90vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Keyboard
-        key={keyboardKey}
-        currentWord={currentWordObj.word.toUpperCase()}
-        currentHint={currentWordObj.hint}
-        handleRefresh={handleRefresh}
-      />
-      <button onClick={() => console.log(currentWordObj.word)}>
-        console answer
-      </button>
-      <span>{currentWordObj.hint}</span>
+    <div className="main">
+      <div>info container</div>
+      <div>stickman</div>
+      <div
+        className="char-container"
+        style={{ display: 'flex', gap: '2rem' }}>
+        {wordObj.word
+          .toUpperCase()
+          .split('')
+          .map((char, index) => (
+            <div
+              key={index}
+              className="char-box"
+              style={{ padding: '0.5rem', borderBottom: 'solid 5px black' }}>
+              <span
+                key={index}
+                className={`char-span ${char}-char`}
+                style={{ display: 'none', color: 'red' }}>
+                {char}
+              </span>
+            </div>
+          ))}
+      </div>
+      <div>
+        attempts left: {attempts} &nbsp;{' '}
+        <button onClick={() => console.log(wordObj.word)}>answer</button>
+        <button onClick={() => console.log(charLeft)}>char left</button>
+      </div>
+      <div className="keyboard-container">
+        <Keyboard
+          key={keyboardKey}
+          userInput={setUserInput}
+          handleRefresh={handleRefresh}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,89 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 const keyboardRows: string[] = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
 
 type KeyboardProps = {
-  currentWord: string;
-  currentHint: string;
+  userInput: (key: string) => void;
   handleRefresh: () => void;
 };
 
-const Keyboard: React.FC<KeyboardProps> = ({ currentWord, handleRefresh }) => {
-  const [attempts, setAttempts] = useState(7);
-  const [charLeft, setCharLeft] = useState(currentWord.split(''));
-  const [status, setStatus] = useState<'playing' | 'lost' | 'won'>('playing');
-  const charBtns = document.getElementsByClassName('char-btn');
-  const charSpans = document.getElementsByClassName(
-    'char-span'
-  ) as HTMLCollectionOf<HTMLSpanElement>;
-
+const Keyboard: React.FC<KeyboardProps> = ({ userInput, handleRefresh }) => {
   useEffect(() => {
-    if (!attempts) {
-      setStatus('lost');
-      for (let i = 0; i < charBtns.length; i++) {
-        charBtns.item(i)?.setAttribute('disabled', 'true');
+    // handle keyboard input
+    const handleKeypress = (e: globalThis.KeyboardEvent) => {
+      if (e.key == ' ') {
+        handleRefresh();
+      } else {
+        /^[a-zA-Z]$/.test(e.key) && userInput(e.key);
       }
-      for (let i = 0; i < charSpans.length; i++) {
-        let charSpan = charSpans.item(i);
-        if (charSpan) charSpan.style.display = 'inline-block';
-      }
-    }
-    if (!charLeft.length) {
-      setStatus('won');
-      for (let i = 0; i < charBtns.length; i++) {
-        charBtns.item(i)?.setAttribute('disabled', 'true');
-      }
-    }
-  }, [attempts, charLeft]);
+    };
 
-  function handleInput(e: React.MouseEvent<HTMLButtonElement>) {
-    e.currentTarget.setAttribute('disabled', 'true'); // Disable the respective btn ele
-    const userInput = e.currentTarget.value;
+    window.addEventListener('keypress', handleKeypress);
 
-    if (currentWord.split('').includes(userInput)) {
-      // Correct guess logic
-
-      let coll: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName(
-        `${userInput}-char`
-      ) as HTMLCollectionOf<HTMLElement>;
-
-      for (let i = 0; i < coll.length; i++) {
-        const item = coll.item(i);
-        if (item) {
-          item.style.display = 'inline-block';
-          item.style.color = 'green';
-        }
-      }
-
-      setCharLeft((prev) => prev.filter((char) => char != userInput));
-    } else {
-      // Incorrect guess logic
-
-      setAttempts((prev) => prev - 1);
-    }
-  }
+    return () => {
+      window.removeEventListener('keypress', handleKeypress);
+    };
+  }, []);
 
   return (
-    <div>
-      <div
-        className="char-container"
-        style={{ display: 'flex', gap: '2rem' }}>
-        {currentWord.split('').map((char, index) => (
-          <div
-            key={index}
-            className="char-box"
-            style={{ padding: '0.5rem', borderBottom: 'solid 5px black' }}>
-            <span
-              key={index}
-              className={`char-span ${char}-char`}
-              style={{ display: 'none', color: 'red' }}>
-              {char}
-            </span>
-          </div>
-        ))}
-      </div>
-      <span style={{ margin: '1rem' }}>Attempts Left: {attempts}</span>
-      <span>Status:{status}</span>
+    <>
       {keyboardRows.map((row, index) => (
         <div
           className="row"
@@ -91,20 +34,17 @@ const Keyboard: React.FC<KeyboardProps> = ({ currentWord, handleRefresh }) => {
           {row.split('').map((char, index) => (
             <button
               key={index}
+              id={`${char}-btn`}
               className="char-btn"
               value={char}
-              onClick={(e) => handleInput(e)}>
+              onClick={(e) => userInput(e.currentTarget.value)}>
               {char}
             </button>
           ))}
         </div>
       ))}
-      <button
-        value={'SPACEBAR'}
-        onClick={handleRefresh}>
-        spacebar
-      </button>
-    </div>
+      <button onClick={handleRefresh}>spacebar</button>
+    </>
   );
 };
 
